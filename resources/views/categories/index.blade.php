779751085d797075cd12dc905580fd92f4ce88ab
ahiru401066 <ichikawatomoya0705@gmail.com>
2023-08-05 14:13:59 +0900

    カテゴリー毎の一覧ページ作成

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -10,6 +10,7 @@
         <h1>Blog Name</h1>
         <a href="/posts/create">create</a>
         <div class='posts'>
+            <h1>カテゴリー</h1>
             @foreach ($posts as $post)
                 <div class='post'>
                     <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
